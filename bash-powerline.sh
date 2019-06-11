6744e615781a9f77bb90af21138ca17b1ffd7def
Owen Xie <owenxie24@gmail.com>
2019-06-10 23:54:51 -0400

    Spaghetti bash code to enable indirect (?) assignment
    Some recustomization

--- bash-powerline.sh
+++ bash-powerline.sh
@@ -11,40 +11,33 @@
 
 __powerline() {    
     # Colorscheme
-    readonly RESET='\[\033[m\]'
-
-    readonly SYMBOL_GIT_BRANCH='⑂'
-    readonly SYMBOL_GIT_MODIFIED='*'
+    # readonly SYMBOL_GIT_BRANCH='⑂'
     readonly SYMBOL_GIT_PUSH='↑'
     readonly SYMBOL_GIT_PULL='↓'
 
-    readonly BASE_BG_COLOR=44
+    readonly BASE_BG_COLOR=46
     readonly BASE_BG_EXTRA=""
-    readonly BASE_FG="1;37"
+    readonly BASE_FG='1;37'
     readonly BASE_STR="\u"
 
     readonly CWD_BG_COLOR=45
     readonly CWD_BG_EXTRA=""
-    readonly CWD_FG="1;37"
+    readonly CWD_FG='1;37'
 
-    readonly GIT_BG_COLOR=47
-    readonly GIT_BG_EXTRA=""
-    readonly GIT_FG="1;30"
+    readonly GIT_BG_COLOR=100
+    readonly GIT_BG_EXTRA=''
+    readonly GIT_FG='1;37'
+    readonly GIT_FG_MODIFIED='1;3;31'
+    readonly SYMBOL_GIT_MODIFIED=''
 
     readonly PS_ZERO_BG_COLOR=42
     readonly PS_ERR_BG_COLOR=41
-    readonly PS_ZERO_BG_EXTRA=""
-    readonly PS_ERR_BG_EXTRA=""
-    readonly PS_ZERO_FG="1;37"
-    readonly PS_ERR_FG="1;37"
+    readonly PS_ZERO_BG_EXTRA=''
+    readonly PS_ERR_BG_EXTRA=''
+    readonly PS_ZERO_FG='1;37'
+    readonly PS_ERR_FG='1;37'
 
     readonly PS_DIVIDER=""
-    
-    readonly BASE_STYLE="1;37;44"
-    readonly CWD_STYLE="1;37;45"
-    readonly GIT_STYLE="30;46"
-    readonly PS_STYLE_ZERO="1;37;42"
-    readonly PS_STYLE_ERR="1;37;41"
 
     if [[ -z "$PS_SYMBOL" ]]; then
       case "$(uname)" in
@@ -53,23 +46,6 @@ __powerline() {
           *)        PS_SYMBOL='%';;
       esac
     fi
-
-    # Takes the text, block_bg, block_fg, expandable boolean
-    __block() {
-	if [[ -n $1 ]]; then
-	    if [[ -n "$__LAST_BG" ]]; then
-		if [[ $POWERLINE_OVERLAP = 0 ]]; then
-		    printf "%s" "$PS_DIVIDER"
-		else
-		    local __divider_fg=$((__LAST_BG - 10))
-		    printf "\[\033[%s;%sm\]%s" "$__divider_fg" "$2" "$PS_DIVIDER"
-		fi
-	    fi
-	    
-	    printf "\[\033[%s;%sm\] %s \[\033[m\]" "$2" "$3" "$1"
-	    return $2
-	fi
-    }
 
     __git_info() { 
         [[ $POWERLINE_GIT = 0 ]] && return # disabled
@@ -97,13 +73,40 @@ __powerline() {
                 [[ $line =~ ahead\ ([0-9]+) ]] && marks+=" $SYMBOL_GIT_PUSH${BASH_REMATCH[1]}"
                 [[ $line =~ behind\ ([0-9]+) ]] && marks+=" $SYMBOL_GIT_PULL${BASH_REMATCH[1]}"
             else # branch is modified if output contains more lines after the header line
-                marks="$SYMBOL_GIT_MODIFIED$marks"
-                break
+		printf "$ref$SYMBOL_GIT_MODIFIED$marks"
+		return 1
             fi
         done < <($git_eng status --porcelain --branch 2>/dev/null)  # note the space between the two <
 
         # print the git branch segment without a trailing newline
         printf "$ref$marks"
+    }
+
+    # Adds to PS1
+    # Takes the text, block_bg, block_fg, redirect boolean (for security reasons)
+    __block() {
+	if [[ -n $1 ]]; then
+	    if [[ -n "$__LAST_BG" ]]; then
+		if [[ $POWERLINE_OVERLAP = 0 ]]; then
+		    PS1+="$PS_DIVIDER"
+		else
+		    local __divider_fg=$((__LAST_BG - 10))
+		    PS1+="\[\033[$__divider_fg;$2m\]$PS_DIVIDER"
+		fi
+	    fi
+
+	    PS1+="\[\033[$2;$3m\] "
+	    shopt -q promptvars
+	    if [[ $? && $4 -eq 1 ]]; then
+		__redirect+="$1"
+		PS1+="\${__redirect[$__redirect_counter]}"
+		__redirect_counter+=1
+	    else
+		PS1+="$1"
+	    fi
+	    PS1+=" \[\033[m\]"
+	    __LAST_BG=$2
+	fi
     }
 
     ps1() {
@@ -119,26 +122,24 @@ __powerline() {
 	unset __LAST_BG
 
         local cwd="\w"
-        # Bash by default expands the content of PS1 unless promptvars is disabled.
-        # We must use another layer of reference to prevent expanding any user
-        # provided strings, which would cause security issues.
-        # POC: https://github.com/njhartwell/pw3nage
-        # Related fix in git-bash: https://github.com/git/git/blob/9d77b0405ce6b471cb5ce3a904368fc25e55643d/contrib/completion/git-prompt.sh#L324
-        if shopt -q promptvars; then
-            __powerline_git_info="$(__git_info)"
-            local git="\${__powerline_git_info}"
-        else
-            # promptvars is disabled. Avoid creating unnecessary env var.
-            local git="$(__git_info)"
-        fi
+        
+	__redirect=()
+	__redirect_counter=0
+
+	git="$(__git_info)"
+	if [[ $? -eq 1 ]]; then
+	    local git_style="$GIT_FG_MODIFIED"
+	else
+	    local git_style="$GIT_FG"
+	fi
 	
-	PS1="$(__block $BASE_STR $BASE_BG_COLOR $BASE_FG)"
-	__LAST_BG=$?
-	PS1+="$(__block $cwd $CWD_BG_COLOR $CWD_FG)"
-	__LAST_BG=$?
-	PS1+="$(__block $git $GIT_BG_COLOR $GIT_FG)"
-	__LAST_BG=$?
-	PS1+="$(__block $PS_SYMBOL ${style[0]} ${style[1]}) "
+	# Generate PS1
+	PS1=""
+	__block "$BASE_STR" "$BASE_BG_COLOR" "$BASE_FG"
+	__block "$cwd" "$CWD_BG_COLOR" "$CWD_FG"
+	__block "$git" "$GIT_BG_COLOR" "$git_style" 1
+	__block "$PS_SYMBOL" "${style[0]}" "${style[1]}"
+	PS1+=" "
     }
 
     PROMPT_COMMAND="ps1${PROMPT_COMMAND:+; $PROMPT_COMMAND}"
